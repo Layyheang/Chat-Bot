9cb6034e7abc45d7f8885cedcbfe12ef91e2f04e
Layheang <hak.layheang277@gmail.com>
2025-02-05 19:37:17 +0700

    layheang add Register Account

--- src/views/app-login/components/login-form.vue
+++ src/views/app-login/components/login-form.vue
@@ -4,28 +4,27 @@ import { computed, ref } from "vue";
 import InputText from "primevue/inputtext";
 import Password from "primevue/password";
 import Button from "primevue/button"; 
-import { required, minLength, helpers } from "@vuelidate/validators";
+import { required, helpers } from "@vuelidate/validators";
 import useVuelidate from "@vuelidate/core";
 import { useRouter } from "vue-router"; 
 // Define reactive fields
-const username = ref("");
+const email = ref("");
 const password = ref("");
 const isloading = ref(false);
 const router = useRouter(); 
 const rules = computed(() => {
     return {
-        username: {
-            required: helpers.withMessage("Username is required", required)
+        email: {
+            required: helpers.withMessage("email is required", required)
         },
 
         password: {
             required: helpers.withMessage("Password is required", required),
-            minLength: helpers.withMessage("Password must be at least 12 characters long", minLength(12))
         }
     }
 })
 
-const v$ = useVuelidate(rules, { username, password });
+const v$ = useVuelidate(rules, { email, password });
 
 const login = async () => {
     try {
@@ -49,7 +48,7 @@ const login = async () => {
 
 <template>
     <div class="login-form">
-        <div class="flex items-center justify-center p-[1rem] h-[70vh]">
+        <div class="flex flex-col items-center justify-center p-[1rem] h-[70vh]">
             <div class="bg-form w-[425px] flex flex-col h-[525px] rounded-md">
                 <div class="flex flex-col items-center justify-center p-[2rem] mt-8">
                     <div class="text-white text-center">
@@ -61,12 +60,12 @@ const login = async () => {
                     @keyup.enter="login">
                     <div class="mb-[1rem] flex flex-col">
                         <span class="w-[300px] flex input-field">
-                            <InputText id="username" v-model="username" type="text" class="input-field-inputtext"
-                                placeholder="Username" />
+                            <InputText id="email" v-model="email" type="text" class="input-field-inputtext"
+                                placeholder="Email" />
                             <i class="pi pi-user input-field-icon" />
                         </span>
-                        <small v-if="v$.username.$error" class="text-red-500 mt-1">
-                            {{ v$.username.$errors[0]?.$message }}
+                        <small v-if="v$.email.$error" class="text-red-500 mt-1">
+                            {{ v$.email.$errors[0]?.$message }}
                         </small>
                     </div>
                     <div class="w-[300px] flex flex-col mt-[2rem]">
@@ -78,9 +77,10 @@ const login = async () => {
                             {{ v$.password.$errors[0]?.$message }}
                         </small>
                     </div>
-                    <div class="flex flex-col items-center justify-center p-[5rem]">
+                    <div class="flex flex-col items-center justify-center p-[3rem]">
                         <Button label="Login" rounded outlined @click="login" :loading="isloading"
                             class="bg-[#156896] w-[8rem] h-[3rem] text-white shadow-lg shadow-[rgba(32,28,28,0.4)] hover:shadow-lg hover:shadow-cyan-500/50" />
+                            <Button class="flex  mt-9 text-white" @click="register">Register Account</Button>
                     </div>
                 </div>
             </div>
